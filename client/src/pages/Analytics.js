d2f11dca3944bb84e45301553d6dc18a6b83a788
jyoung369 <julieyoung360@gmail.com>
2024-11-02 03:15:28 -0400

    added comments

--- client/src/pages/Analytics.js
+++ client/src/pages/Analytics.js
@@ -21,19 +21,27 @@ function Analytics() {
     const [query, setQuery] = useState('');
     const[history, setMessageHistory] = useState([{role: 'user', parts: [{text: data}]}])
 
+    //Function to navigate back to home
     const returnToPreviousPage = () => {
         navigate("/home");
     }
 
     //Code to handle a submitted query by 1) Updating the chat history and 2) calling the genAI API
     const handleMessage = async () => {
+        //Create an LLM chat using the saved history
         const chat = model.startChat({
             history: history
         })
+
+        //Save the question asked by the user
         const question = query;
         setQuery('');
+
+        //Update history
         const newHistory = [...history, {role: 'user', parts: [{text: question}]}];
         setMessageHistory(newHistory);
+        
+        //Send user question to LLM and store result in history
         const result = await chat.sendMessage(question);
         setMessageHistory([...newHistory, {role: 'model', parts: [{text: result.response.text()}]}]);
     }
