908e604f84ac175832b37d6d1d2d8b629ad52215
ADPappas <alexisdpappas@gmail.com>
2024-11-02 07:05:28 -0400

    add photos to dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,71 +2,85 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import 'bulma/css/bulma.min.css';
 import Navbar from '../components/Navbar';
+import ehrImage from '../components/ehrPicture.png';
+import hrisImage from '../components/HRISPicture.png';
+import hotlineImage from '../components/988.png';
+import phone from '../components/phone.png';
+import finances from '../components/finances.png';
 
 const DatabaseSelector = () => {
-   const navigate = useNavigate();
+ const navigate = useNavigate();
 
-   const handleHomeClick = () => {
-       navigate("/home");
-   };
+ const handleHomeClick = () => {
+     navigate("/home");
+ };
 
-   return (
-       <>
-           <Navbar />
-           <div className="container">
-               <h1 className="title is-3 has-text-centered">Choose a Database</h1>
-               <div className="columns is-multiline">
-                   {/* EHR Database Tile */}
-                   <div className="column is-one-third">
-                       <div className="box has-text-centered" onClick={handleHomeClick} style={{ cursor: 'pointer' }}>
-                           <h2 className="title is-4">EHR Report</h2>
-                           <p>View EHR report data</p>
-                       </div>
-                   </div>
+ return (
+     <>
+         <Navbar />
+         <div className="container">
+             <h1 className="title is-3 has-text-centered">Choose a Database</h1>
+             <div className="columns is-multiline">
+                 {/* EHR Database Tile */}
+                 <div className="column is-one-third">
+                     <div className="box has-text-centered" onClick={handleHomeClick} style={{ cursor: 'pointer' }}>
+                         <h2 className="title is-4">EHR System</h2>
+                         <div style={{ width: '250px', height: '150px', overflow: 'hidden', margin: '0 auto' }}>
+                         <img src={ehrImage} alt="EHR tile" style={{ maxWidth: '100%', height: 'auto' }} />
+                         </div>
+                     </div>
+                 </div>
 
-                   {/* Database Tile 2 */}
-                   <div className="column is-one-third">
-                       <div className="box has-text-centered">
-                           <h2 className="title is-4">Database 2</h2>
-                           <p>Description about Database 2</p>
-                       </div>
-                   </div>
+                 {/* Financial Database */}
+                 <div className="column is-one-third">
+                     <div className="box has-text-centered">
+                         <h2 className="title is-4">Financial System</h2>
+                         <div style={{ width: '250px', height: '150px', overflow: 'hidden', margin: '0 auto' }}>
+                              <img src={finances} alt="HRIS tile" style={{ maxWidth: '100%', height: 'auto', objectFit: 'cover' }} />
+                         </div>
+                     </div>
+                 </div>
 
-                   {/* Database Tile 3 */}
-                   <div className="column is-one-third">
-                       <div className="box has-text-centered">
-                           <h2 className="title is-4">Database 3</h2>
-                           <p>Description about Database 3</p>
-                       </div>
-                   </div>
+                 {/* HRIS Database */}
+                 <div className="column is-one-third">
+                     <div className="box has-text-centered">
+                         <h2 className="title is-4">HRIS System</h2>
+                         <div style={{ width: '250px', height: '150px', overflow: 'hidden', margin: '0 auto' }}>
+                              <img src={hrisImage} alt="HRIS tile" style={{ maxWidth: '100%', height: 'auto', objectFit: 'cover' }} />
+                         </div>
+                     </div>
+                 </div>
 
-                   {/* Database Tile 4 */}
-                   <div className="column is-one-third">
-                       <div className="box has-text-centered">
-                           <h2 className="title is-4">Database 4</h2>
-                           <p>Description about Database 4</p>
-                       </div>
-                   </div>
+                  {/* Call logs Database */}
+                  <div className="column is-one-third">
+                     <div className="box has-text-centered">
+                         <h2 className="title is-4">Phone System</h2>
+                         <div style={{ width: '250px', height: '150px', overflow: 'hidden', margin: '0 auto' }}>
+                              <img src={phone} alt="HRIS tile" style={{ maxWidth: '100%', height: 'auto', objectFit: 'cover' }} />
+                         </div>
+                     </div>
+                 </div>
 
-                   {/* Database Tile 5 */}
-                   <div className="column is-one-third">
-                       <div className="box has-text-centered">
-                           <h2 className="title is-4">Database 5</h2>
-                           <p>Description about Database 5</p>
-                       </div>
-                   </div>
-
-                   {/* Database Tile 6 */}
-                   <div className="column is-one-third">
-                       <div className="box has-text-centered">
-                           <h2 className="title is-4">Database 6</h2>
-                           <p>Description about Database 6</p>
-                       </div>
-                   </div>
-               </div>
-           </div>
-       </>
-   );
+                 {/* 988 Database */}
+                 <div className="column is-one-third">
+                     <div className="box has-text-centered">
+                         <h2 className="title is-4">988 System</h2>
+                         <div style={{ width: '250px', height: '150px', overflow: 'hidden', margin: '0 auto' }}>
+                              <img src={hotlineImage} alt="HRIS tile" style={{ maxWidth: '100%', height: 'auto', objectFit: 'cover' }} />
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </>
+ );
 };
 
 export default DatabaseSelector;
+
+
+
+
+
+
+
