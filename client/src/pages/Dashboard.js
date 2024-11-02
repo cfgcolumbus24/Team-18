305c50b66ffe25a871711981f97a1fc796bebe8c
ADPappas <alexisdpappas@gmail.com>
2024-11-02 09:03:35 -0400

    edit dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -19,7 +19,7 @@ const DatabaseSelector = () => {
      <>
          <Navbar />
          <div className="container">
-             <h1 className="title is-3 has-text-centered">Choose a Database</h1>
+             <h1 className="title is-3 has-text-centered">Choose the Path to a Patient's Journey</h1>
              <div className="columns is-multiline">
                  {/* EHR Database Tile */}
                  <div className="column is-one-third">
@@ -28,6 +28,7 @@ const DatabaseSelector = () => {
                          <div style={{ width: '250px', height: '150px', overflow: 'hidden', margin: '0 auto' }}>
                          <img src={ehrImage} alt="EHR tile" style={{ maxWidth: '100%', height: 'auto' }} />
                          </div>
+                         <p style={{marginTop: '10px'}}>View data from electronic health records</p>
                      </div>
                  </div>
 
@@ -38,6 +39,7 @@ const DatabaseSelector = () => {
                          <div style={{ width: '250px', height: '150px', overflow: 'hidden', margin: '0 auto' }}>
                               <img src={finances} alt="HRIS tile" style={{ maxWidth: '100%', height: 'auto', objectFit: 'cover' }} />
                          </div>
+                         <p style={{marginTop: '10px'}}>Gather information on financial reports, assess budgets</p>
                      </div>
                  </div>
 
@@ -48,6 +50,7 @@ const DatabaseSelector = () => {
                          <div style={{ width: '250px', height: '150px', overflow: 'hidden', margin: '0 auto' }}>
                               <img src={hrisImage} alt="HRIS tile" style={{ maxWidth: '100%', height: 'auto', objectFit: 'cover' }} />
                          </div>
+                         <p style={{marginTop: '10px'}}>View employee records and management tools</p>
                      </div>
                  </div>
 
@@ -58,6 +61,7 @@ const DatabaseSelector = () => {
                          <div style={{ width: '250px', height: '150px', overflow: 'hidden', margin: '0 auto' }}>
                               <img src={phone} alt="HRIS tile" style={{ maxWidth: '100%', height: 'auto', objectFit: 'cover' }} />
                          </div>
+                         <p style={{marginTop: '10px'}}>View past call logs between patients and clinicians</p>
                      </div>
                  </div>
 
@@ -66,8 +70,9 @@ const DatabaseSelector = () => {
                      <div className="box has-text-centered">
                          <h2 className="title is-4">988 System</h2>
                          <div style={{ width: '250px', height: '150px', overflow: 'hidden', margin: '0 auto' }}>
-                              <img src={hotlineImage} alt="HRIS tile" style={{ maxWidth: '100%', height: 'auto', objectFit: 'cover' }} />
+                              <img src={hotlineImage} alt="HRIS tile" style={{ maxWidth: '100%', height: 'auto', objectFit: 'cover'}} />
                          </div>
+                         <p style={{marginTop: '10px'}}>See chat metrics from the 988 Suicide & Crisis Lifeline</p>
                      </div>
                  </div>
              </div>
@@ -77,10 +82,3 @@ const DatabaseSelector = () => {
 };
 
 export default DatabaseSelector;
-
-
-
-
-
-
-
