fc5235efa347455695451351e73dcf85543631ec
akshayam <akshayamukkala@gmail.com>
2024-11-02 01:43:50 -0400

    report page

--- client/src/pages/Report.js
+++ client/src/pages/Report.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import jsPDF from 'jspdf';
 
 function Report() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const returnHome = () => {
+    navigate("/home");
+  };
 
-    const returnToPreviousPage = () => {
-        navigate("/home");
-    }
+  const generatePDF = () => {
+    const doc = new jsPDF();
+    doc.text("This is a blank PDF document.", 10, 10);
+    doc.save("blank.pdf");
+  };
 
   return (
-    <div>
-      <h1>Report Page</h1>
-      <button onClick={returnToPreviousPage}>Back</button>
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1 className="title has-text-centered has-text">Report</h1>
+      <button className="button" onClick={returnHome}>Back</button>
+      <button className="button is-info" onClick={generatePDF} style={{ marginLeft: '1rem' }}>
+        Export PDF
+      </button>
     </div>
   );
 }
