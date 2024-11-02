db7e139b53086ec99018bebea5b0c8bcb2949125
Eli L <118549410+amazingEPIC@users.noreply.github.com>
2024-11-02 02:14:20 -0400

    Revert "Feature/datavisuals"

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
+import Report from './pages/Report';
 import Analytics from './pages/Analytics';
-import HardcodedChart from './pages/datavisual';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/analytics' element={<Analytics />} />
-          <Route path='/datavisual' element={<HardcodedChart />} /> {/* Corrected casing */}
+          <Route path='/' element={<Home />}/>
+          <Route path='/report' element={<Report />}/>
+          <Route path='/analytics' element={<Analytics />}/>
         </Routes>
       </BrowserRouter>
     </div>
@@ -20,4 +20,3 @@ function App() {
 }
 
 export default App;
-
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,21 +5,19 @@ function Home() {
 
     const navigate = useNavigate();
 
-    
+    const handleReportClick = () => {
+        navigate("/report");
+    }
 
     const handleAnalyticClick = () => {
         navigate("/analytics");
     }
 
-    const handleDataVisualClick = () => {
-        navigate("/datavisual");
-    }
-
   return (
     <div>
       <h1>Home Page</h1>
+      <button onClick={handleReportClick}>Go To Report</button>
       <button onClick={handleAnalyticClick}>Go To Analytics</button>
-      <button onClick={handleDataVisualClick}>Go To Data Visual</button>
     </div>
   );
 }
